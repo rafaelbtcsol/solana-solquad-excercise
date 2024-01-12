0dcc1b66ddae0555e524e6dc116908c541ddf0e9
rafaelbtcsol <156289525+rafaelbtcsol@users.noreply.github.com>
2024-01-12 13:30:38 +0700

    update solution

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("C98qSxsA9VYicvbviDPdTtUAGBwN5RtohYKoPkqWT29H");
 
 #[program]
 pub mod solquad {
